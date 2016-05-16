bdda60f696edfba1abfb6decbea1f45aa5e14792
851085835@qq.com <851085835@qq.com>
2016-05-16 09:49:03 +0800

    jsss

---
+++ JS交互/Deep JavaScriptWithOC/Deep JavaScriptWithOC/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Deep JavaScriptWithOC
+//
+//  Created by Aotu on 16/4/28.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ JS交互/Deep JavaScriptWithOC/Deep JavaScriptWithOC/ShowView.h
@@ -0,0 +1,16 @@
+//
+//  ShowView.h
+//  Deep JavaScriptWithOC
+//
+//  Created by Aotu on 16/5/6.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowView : UIView
+
+-(instancetype)createWithFrame:(CGRect)frame backGroundcolor:(UIColor*)Bcakcolor;
+
+
+@end
